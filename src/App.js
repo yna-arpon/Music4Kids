f235d6d25edc9c2d50f749d1522509fe768362ca
yna-arpon <104539097+yna-arpon@users.noreply.github.com>
2022-06-06 14:32:37 -0600

    Added graphics to start screen background

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <StartScreen />
         <ModuleOptns />
       </div>
-      
     </div>
   );
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import Photo from './img.png'
+import Photo from './band.png'
+import Vector1 from './vector1.png'
+import Vector2 from './vector2.png'
 
 const Header = () => {
   return (
     <div>
-      <div>
-        <img className='band' src={ Photo } alt='Band' />
-        <h1 className='title'>MUSIC<span className='forKids' style={{color: '#FFA723'}}>4KIDS</span></h1>
-        <h1 className='subtitle'>Let's make some music!</h1>
+      <div className='vector1'> 
+        <img src={ Vector1 } alt='vector1' />
       </div>
+      <div className='vector2'>
+        <img  src = { Vector2 } alt='vector2' />
+      </div>
+      <img className='band' src={ Photo } alt='Band' />
+      <h1 className='title'>MUSIC<span className='forKids' style={{color: '#FFA723'}}>4KIDS</span></h1>
+      <h1 className='subtitle'>Let's make some music!</h1>
     </div>   
   )
 }
