b42c7392143f507597d0e820d3d592fa99b7ddb4
Yna Arpon <ynaarpon4@gmail.com>
2022-06-24 12:18:16 -0600

    Created black keys

--- src/components/PianoChords.js
+++ src/components/PianoChords.js
@@ -12,7 +12,8 @@ class Sketch extends React.Component {
     // This uses p5's instance mode for sketch creation and namespacing
     Sketch = (p) => {
 
-        let whiteKeys, C4, D4, E4, F4, G4, A4, B4, C5, D5, E5, F5, G5, A5, B5;
+        let whiteKeys, C4, D4, E4, F4, G4, A4, B4, C5, D5, E5, F5, G5, A5, B5, whiteKeyWidth, whiteKeyHeight;
+        let blackKeys, C4s, D4s, F4s, G4s, A4s, C5s, D5s, F5s, G5s, A5s;  
 
         p.setup = () => {
             p.createCanvas(p.windowWidth,  p.windowHeight);
@@ -29,20 +30,46 @@ class Sketch extends React.Component {
             }
             
             whiteKeys = [C4, D4, E4, F4, G4, A4, B4, C5, D5, E5, F5, G5, A5, B5]
+            whiteKeys.forEach(createWhiteKey)
 
-            whiteKeys.forEach(createKey)
+            blackKeys = [C4s, D4s, F4s, G4s, A4s, C5s, D5s, F5s, G5s, A5s];
+            blackKeys.forEach(createBlackKey)
 
+            
         }
 
-        function createKey() { 
+        function createWhiteKey() { 
             let xPos = 0;
 
             for(let i = 0; i < 14; i++) {
+                whiteKeyWidth = p.windowWidth / 14;
+                whiteKeyHeight = p.windowHeight / 2.33;
+
                 whiteKeys[i] = p.createButton('');
-                whiteKeys[i].size(107.1, 367.8);
+                whiteKeys[i].size(whiteKeyWidth, whiteKeyHeight);
                 whiteKeys[i].style('background-color', p.color(255));
-                whiteKeys[i].position(xPos, 489.2);
-                xPos += 107.1;
+                whiteKeys[i].position(xPos, p.windowHeight - whiteKeyHeight);
+                
+                xPos += whiteKeyWidth;
+            }
+        }
+
+        function createBlackKey() {
+            let blackKeyWidth = 0.553 * whiteKeyWidth;
+            let blackKeyHeight = 0.6197 * whiteKeyHeight; 
+            let xPos = whiteKeyWidth - (blackKeyWidth * 0.5);
+
+            for(let i = 0; i < 10; i++) {
+                blackKeys[i] = p.createButton('');
+                blackKeys[i].size(blackKeyWidth, blackKeyHeight);
+                blackKeys[i].style('background-color', p.color(0));
+                blackKeys[i].position(xPos, p.windowHeight - whiteKeyHeight)
+
+                if (i == 2 || i == 5 || i == 7) {
+                    xPos += 2 * whiteKeyWidth;
+                } else {
+                    xPos += whiteKeyWidth
+                }
             }
         }
 
